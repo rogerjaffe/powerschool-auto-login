7ec9c620d652638491c8c7b00658ff0d5902ec38
Roger Jaffe <rogerjaffe@gmail.com>
2024-07-09 16:46:12 -0700

    Update for slight changes in webpage protocol

--- ExtensionFiles/powerSchoolLogin.js
+++ ExtensionFiles/powerSchoolLogin.js
@@ -1,12 +1,14 @@
 var currentURL = document.location.href;
-if (currentURL === "https://powerschool.sandi.net/teachers/pw.html") {
+if (currentURL.startsWith("https://adfs19.sandi.net/adfs")) {
 	chrome.storage.sync.get({
     username: '',
     password: ''
   }, function(items) {
-		document.querySelector('input[name="username"]').value=items.username; // District ID
-		document.querySelector('input[name="password"]').value=decrypt(items.password); // District password
-		document.querySelector('#btnEnter').click();
+		document.querySelector('input[name="UserName"]').value=items.username; // District ID
+		document.querySelector('input[name="Password"]').value=decrypt(items.password); // District password
+		setTimeout(() => {
+			document.querySelector('#submitButton').click();
+		}, 1000);
   });
 }
 
